26ec6105b9d00d970280bdb8ed5c9b344f5f253e
Mario Vega <mario@ceibaweb.com>
2017-05-12 21:13:02 -0700

    begin JSONImporter addon

---
+++ site/addons/JSONImporter/JSONImporterTags.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Statamic\Addons\JSONImporter;
+
+use Statamic\Extend\Tags;
+
+class JSONImporterTags extends Tags
+{
+    /**
+     * The {{ j_s_o_n_importer }} tag
+     *
+     * @return string|array
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * The {{ j_s_o_n_importer:example }} tag
+     *
+     * @return string|array
+     */
+    public function example()
+    {
+        //
+    }
+}
---
+++ site/addons/JSONImporter/resources/assets/js/fieldtype.js
@@ -0,0 +1,25 @@
+Vue.component('j_s_o_n_importer-fieldtype', {
+
+    template: '<div>JSONImporter</div>',
+
+    props: ['data', 'config', 'name'],
+
+    data: function() {
+        return {
+            //
+        };
+    },
+
+    computed: {
+        //
+    },
+
+    methods: {
+        //
+    },
+
+    ready: function() {
+        //
+    }
+
+});
