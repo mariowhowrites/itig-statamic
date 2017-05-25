7c6ec3be6faed8bb4384a185b77db9da204c342a
Mario Vega <mario@ceibaweb.com>
2017-05-25 10:06:54 -0700

    Refactor listener, add form submissions to gitignore

--- site/addons/StatamicCampaign/StatamicCampaignListener.php
+++ site/addons/StatamicCampaign/StatamicCampaignListener.php
@@ -18,20 +18,19 @@ class StatamicCampaignListener extends Listener
     public function sendToActiveCampaign(Submission $submission) {
         $ac = new \ActiveCampaign( 'https://itig.api-us1.com', 'a16cd0653ce4eb18e6614a9b8bf61b4b4513aa76f172ff49311896cfb7c32c7ab749a280');
 
-        $body = collect( array_keys($submission->formset()->fields() ) )
-                ->map(
-                 function($field) use ($submission) {
-                    return $field =  $submission->get($field);
-                });
+        if ('ac-hero' === $submission->formset()->name() ) {
+            $body = collect( array_keys( $submission->formset()->fields() ) )
+                    ->reduce( function($array, $field) use ($submission) {
+                        $array[$field]  = $submission->get($field);
+                        return $array;
+                    }, []);
 
-        dd($body);
-        $response = $ac->api('contact/sync', [
-            'email'         =>  $submission->get('email'),
-            'first_name'     =>  $submission->get('first_name'),
-            'last_name'    =>  $submission->get('last_name'),
-            'list_id'       =>  'p[3]',
-            ]
-        );
+            $body['list_id'] = 'p[3]';
+
+            $body;
+
+            $response = $ac->api('contact/sync', $body);
+        }
 
 
 
